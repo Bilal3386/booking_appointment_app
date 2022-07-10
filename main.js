f70e9dad434c0bd2d93aeeb7c520fcf13fb71e89
mohdbilal <mohdbilal3386@gmail.com>
2022-07-11 01:51:47 +0530

    let's_store_the_users_in_the_locale_stroge

--- main.js
+++ main.js
@@ -12,10 +12,41 @@ btn.addEventListener('click', (e) => {
     {
         alert('Please enter value to every field')
     }else {
-    localStorage.setItem('Name:', name.value )
-    localStorage.setItem('Email:', email.value )
-    localStorage.setItem('Phone:', phone.value)
-    localStorage.setItem('Date:',  date.value)
-    localStorage.setItem('Time:',  time.value)
+
+               /*storing single user Details in the local storage */
+    // localStorage.setItem('Name:', name.value )
+    // localStorage.setItem('Email:', email.value )
+    // localStorage.setItem('Phone:', phone.value)
+    // localStorage.setItem('Date:',  date.value)
+    // localStorage.setItem('Time:',  time.value)
+
+
+               /*storing user Details as an object in the local storage */
+    // create an object and enter the keys and its' values 
+    let obj = {
+        personName: name.value,
+        personEmail: email.value,
+        personPhone: phone.value,
+        personMeetDate: date.value,
+        personMeetTime: time.value 
+    }
+    // setting object to localStorage
+    localStorage.setItem('obj',JSON.stringify(obj))
     }
 })
+
+// details will pop up after refresh on the screen
+let data = document.createElement('p')
+data.className = "details"
+
+
+// create a getItem
+let obj_details = JSON.parse(localStorage.getItem('obj'))
+// add details to para
+let temp = " "
+ for(let x in obj_details)
+ {
+    temp += x +': ' +obj_details[x];
+}
+data.textContent = temp
+document.querySelector('.container').appendChild(data)
